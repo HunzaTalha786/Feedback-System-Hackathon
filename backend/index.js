28439b6801d08924164a7cdb78648c2022bb1932
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2025-07-27 22:15:50 +0500

    Done

--- backend/index.js
+++ backend/index.js
@@ -28,9 +28,9 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on http://localhost:${process.env.PORT}`)
-    );
+    // app.listen(process.env.PORT, () =>
+    //   console.log(`Server running on http://localhost:${process.env.PORT}`)
+    // );
   })
   .catch((err) => console.log(err));
 
